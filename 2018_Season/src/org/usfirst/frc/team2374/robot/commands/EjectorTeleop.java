107a387f4a2739c86bfafcedba92188eab906700
robotics <robotics@Y-ROBOTICS03.jesuitpdx.org>
2018-01-11 16:40:13 -0800

    minor bugfix

--- 2018_Season/src/org/usfirst/frc/team2374/robot/commands/EjectorTeleop.java
+++ 2018_Season/src/org/usfirst/frc/team2374/robot/commands/EjectorTeleop.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team2374.robot.commands;
 import org.usfirst.frc.team2374.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
 
 public class EjectorTeleop extends Command {
 
